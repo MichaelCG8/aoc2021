0013e442d61bcd387f5c41b219535821e2be5246
Michael Garbutt <michael.garbutt@pommelhorse.com>
2021-12-07 22:11:49 +0000

    Updating template

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,23 +1,22 @@
-use std::fs;
 use std::time;
 use aoc2021;
 
 
 fn main() {
     let start_total = time::Instant::now();
-    let data = fs::read_to_string("inputs/0").unwrap();
+    let data = include_str!("../../inputs/0");
     let start_part1 = time::Instant::now();
-    // println!("Part 1: {} in {:?}", part1(&data), start_part1.elapsed());
+    println!("Part 1: {} in {:?}", part1(data), start_part1.elapsed());
     let start_part2 = time::Instant::now();
-    // println!("Part 2: {} in {:?}", part2(&data), start_part2.elapsed());
+    // println!("Part 2: {} in {:?}", part2(data), start_part2.elapsed());
 
     println!("Total: {:?}", start_total.elapsed())
 }
 
 
-// fn part1(data: &str) -> isize {
-//
-// }
+fn part1(data: &str) -> isize {
+
+}
 
 
 // fn part2(data: &str) -> isize {
@@ -28,17 +27,15 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    // static DATA : &str = "199
-// 260
-// 263";
+    static DATA : &str = "";
 
     #[test]
     fn part1_matches_sample() {
-        // assert_eq!(part1(DATA), 7);
+        assert_eq!(part1(DATA), );
     }
 
     #[test]
     fn part2_matches_sample() {
-        // assert_eq!(part2(DATA), 5);
+        // assert_eq!(part2(DATA), );
     }
 }
